2ffa938b7f6d580360632d6a805656325dc4e379
Richard King <richardking1345@gmail.com>
2020-08-11 16:30:32 +0100

    First change

---
+++ TestWebApp1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestWebApp1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ TestWebApp1/Views/SimpleClasses/TimeSetGreeting.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace TestWebApp1.Views.Simple_Classes
+{
+    public class TimeSetGreeting
+    {
+        public static string TimeSetWrite()
+        {
+            string timeRightNow = DateTime.Now.ToString("h:mm:ss tt");
+            return timeRightNow;
+        }
+    }
+}
+
+
+
+/*             Console.Write("Current Date and Time is : ");
+            
+*/
